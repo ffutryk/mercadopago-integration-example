8c126685c414585b9ea957423d3a23819fab6236
Fabricio Futryk <ffutryk52@gmail.com>
2025-05-15 04:01:13 -0300

    feat: add secret to validate webhook request authenticity

--- src/config/secrets.js
+++ src/config/secrets.js
@@ -2,3 +2,5 @@ import 'dotenv/config';
 
 export const PORT = process.env.PORT ?? 3000;
 export const NODE_ENV = process.env.NODE_ENV ?? 'dev';
+export const MERCADOPAGO_AUTHENTICITY_SECRET =
+  process.env.MERCADOPAGO_AUTHENTICITY_SECRET;
