0272c400e0cdff71e9b9ac73916bfd8133741e3c
Fabricio Futryk <ffutryk52@gmail.com>
2025-05-15 04:03:06 -0300

    feat: use MercadoPago router and errorHandler middleware

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import { NODE_ENV, PORT } from './config/secrets.js';
+import { errorHandler } from './middlewares/error.middleware.js';
+import { mercadoPagoRouter } from './routes/mercadopago.routes.js';
 
 const app = express();
 
 app.use(morgan(NODE_ENV === 'dev' ? 'dev' : 'combined'));
 app.use(express.json());
 app.use(cors());
+
+app.use('/mercadopago', mercadoPagoRouter);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Ready, ${PORT}`);
